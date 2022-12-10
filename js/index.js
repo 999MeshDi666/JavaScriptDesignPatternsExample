d54884241a2b035baea5010c5dfcd0d5654e1994
999MeshDi666 <havkpzz@gmail.com>
2022-12-10 20:53:48 +0600

    get dif data on next state

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let items = document.getElementById('items');
 let sandwich = JSON.parse(localStorage.getItem('list'))
 
 for(let i = 0; i <sandwich.length; i++){
-    items.innerHTML += `<div class="card" style="width: 18rem;" id="card" data-index=${i}>
+    items.innerHTML += `<div class="card" style="width: 18rem;">
                             <img src=${sandwich[i][1]['src']} class="card-img-top card-img">
                             <div class="card-body">
                                 <h5 class="card-title">${sandwich[i][1]['title']}</h5>
@@ -18,18 +18,34 @@ for(let i = 0; i <sandwich.length; i++){
                                 <li class="list-group-item">Цена: ${sandwich[i][1]['price']}</li>
                             </ul>
                             <div class="card-body">
-                                <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Заказать</button>
+                            <button 
+                                class="btn btn-primary order-btn" 
+                                ${sandwich[i][1]['availability']===true? "":"disabled"} 
+                                data-bs-toggle="modal" data-index=${i} 
+                                data-bs-target="#staticBackdrop">Заказать
+                            </button>
                             </div>
                         </div>`
 }
-
-// card.addEventListener('click', (e)=>{
-//     console.log(e.target.dataset.index)
-// })
-
-let orderBtn = document.getElementById('order-btn');
 const order = new Order();
-orderBtn.addEventListener('click', ()=>{
-    order.nextState();
-    console.log(order.state.name)
+let orderBtns = document.querySelectorAll('.order-btn');
+let modalBody = document.getElementById('modal-body');
+let modalNextBtn = document.getElementById('modal-next-btn');
+let modalPrevBtn = document.getElementById('modal-prev-btn');
+
+orderBtns.forEach((elem)=>
+    elem.addEventListener('click', (e)=>{
+        modalBody.setAttribute('data-index', e.target.dataset.index) 
+        order.orderContent(modalBody)
+    })
+)
+
+modalNextBtn.addEventListener('click', ()=>{
+    order.nextState(modalPrevBtn);
+    order.orderContent(modalBody);
+})
+
+modalPrevBtn.addEventListener('click', ()=>{
+    order.prevState(modalPrevBtn)
+    order.orderContent(modalBody);
 })
--- js/state.js
+++ js/state.js
@@ -1,50 +1,78 @@
 class OrderStatus {
-    modalBody = document.getElementById('modal-body');
-	constructor(name, nextStatus) {
+	constructor(name, nextStatus, prevStatus) {
 		this.name = name;
 		this.nextStatus = nextStatus;
+		this.prevStatus = prevStatus;
 	}
 
 	next() {
 		return new this.nextStatus();
 	}
+	prev(){
+		return new this.prevStatus();
+	}
+	
 }
 
 class StartOrder extends OrderStatus {
 	constructor() {
-       
-		super('order', ConfirmOrder);
-        this.modalBody.innerHTML="StartOrder";
+		super('order', ConfirmOrder, Delivered);
 	}
 }
 
 class ConfirmOrder extends OrderStatus {
 	constructor() {
-        
-		super('confirm', Delivered);
-        this.modalBody.innerHTML="ConfirmOrder";
+		super('confirm', Delivered, StartOrder);
 	}
 }
 
 class Delivered extends OrderStatus {
 	constructor() {
-       
-		super('delivered', Delivered);
-        this.modalBody.innerHTML="Delivered";
+		super('delivered', StartOrder, ConfirmOrder);
 	}
 }
 
 class Order {
+	sandwich = JSON.parse(localStorage.getItem('list'))
 	constructor() {
 		this.state = new StartOrder();
 	}
 
-	nextState() {
+	nextState(modalPrevBtn) { 
 		this.state = this.state.next();
-	};
-    // cancelOrder(){
-    //     this.state.name = 
-    // }
+
+        this.state.name === "confirm" ?
+		modalPrevBtn.style.display = "block": 
+		modalPrevBtn.style.display = "none"
+	}
+	prevState(modalPrevBtn){
+		this.state = this.state.prev();
+		this.state.name === "confirm" ?
+		modalPrevBtn.style.display = "block": 
+		modalPrevBtn.style.display = "none"
+		
+    }
+	orderContent(modalBody){
+
+		if(this.state.name === "order"){
+			let id = modalBody.getAttribute('data-index')
+			modalBody.innerHTML = 
+			`<ul>
+				<li> Title: ${this.sandwich[id][1]['title']}</li>
+				<li> Subtitle: ${this.sandwich[id][1]['subtitle']}</li>
+				<li> Description: ${this.sandwich[id][1]['desc']}</li>
+				<li> Price: ${this.sandwich[id][1]['price']}</li>
+			</ul>`
+		}
+		else if(this.state.name === "confirm"){
+			modalBody.innerHTML = "confirm"
+		}
+		else if(this.state.name === "delivered"){
+			modalBody.innerHTML = "delivered"
+		}
+		
+	}
+   
 }
 
 export {Order}
